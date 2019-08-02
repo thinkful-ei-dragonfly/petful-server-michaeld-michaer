2b3a9a2e753b254e3723677fe250e35c2e9d17da
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-08-02 19:04:17 -0400

    moved routes to catRouter dogRouter

--- src/app.js
+++ src/app.js
@@ -4,49 +4,22 @@ const { CLIENT_ORIGIN } = require('./config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const dogRouter = require('./dogs/DogRouter');
+const catRouter = require('./cats/CatRouter');
 
 const app = express();
-
-const morganOption = (process.env.NODE_ENV === 'production')
+const morganOption = (process.env.NODE_ENV === 'production') // For server side logs
   ? 'tiny'
   : 'common';
-
 app.use(cors({ origin: CLIENT_ORIGIN }));
 app.use(helmet());
-app.use(morgan(morganOption))
+app.use(morgan(morganOption));
 app.use(express.json());
 
-const petRouter = express.Router();
 
-petRouter.route('/api/cat').get((req, res) => {
-  res.json({
-    imageURL:
-      'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-    imageDescription:
-      'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
-    age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
-  });
-});
+app.use('/api/cat', catRouter);
+app.use('/api/dog', dogRouter);
 
-petRouter.route('/api/dog').get((req, res) => {
-  res.json({
-    imageURL:
-      'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription:
-      'A smiling golden-brown golden retriever listening to music.',
-    name: 'Zeus',
-    sex: 'Male',
-    age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
-  });
-});
-
-app.use(petRouter);
 
 // Catch-all 404
 app.use(function(req, res, next) {
